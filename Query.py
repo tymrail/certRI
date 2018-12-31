6e62b0a09ffd339d85f7ab25059c1e3eec46efc2
tymrail <tymrail@163.com>
2018-12-30 23:43:34 -0800

    finally we did sth.

--- Query.py
+++ Query.py
@@ -12,6 +12,8 @@ from Config import Conf
 
 import pickle
 
+from Relevance import calculateBM25
+
 
 class Query(object):
     def __init__(self):
@@ -31,6 +33,14 @@ class Query(object):
         self.lem = WordNetLemmatizer()
         self.stemmer = PorterStemmer()
         self.stopwords = set(stopwords.words("english"))
+
+        self.filter = (
+            [28, 29, 25, 22, 6, 7],
+            [26, 11, 1, 18, 21, 4],
+            [19, 24, 27, 30, 12, 23],
+            [13, 14, 3, 16, 8, 9],
+            [15, 20, 5, 10, 17, 2],
+        )
 
     def getPickles(self, pickle_path):
         with open(pickle_path, "rb") as pf:
@@ -71,11 +81,7 @@ class Query(object):
         )
         return list(lem_tokens_without_stopword)
 
-    def query(self, single_query):
-        gender_lst = ["male", "female"]
-        must_not_gender = gender_lst[abs(gender_lst.index(single_query["gender"]) - 1)]
-        # 性别分为male，female和All三种，得到不用的一种
-
+    def query(self, single_query, name='non_cut'):       
         query_keywords = single_query["disease"].lower().split(" ")
         relevence = single_query["disease"].lower().split(" ")
 
@@ -124,12 +130,45 @@ class Query(object):
         # p10:0.3138
         # p15:0.2704
         # with age: p5: 0.3586 p10:0.3172 p15:0.2805
-        # with gender: p5: 0.3655 p10:0.3241 p15:0.2920
+        # with age and gender: p5: 0.3655 p10:0.3241 p15:0.2920
+
+        # query_base = (single_query["disease"] + ' ') * 3
+        # query_gene = (single_query["gene"].replace("(", " ").replace(")", " ").replace(",", "").replace(";", "") + ' ') * 2
+
+        # query_body = {
+        #     "query": {
+        #         "multi_match": {
+        #             "query": (query_base + ' ' + query_gene).lower(),
+        #             "type": "cross_fields",
+        #             "fields": [
+        #                 "brief_title",
+        #                 "brief_summary",
+        #                 "detailed_description",
+        #                 "official_title",
+        #                 "keyword",
+        #                 "condition",
+        #                 "eligibility.criteria.textblock",
+        #             ],
+        #         }
+        #     },
+        #     "size": 1000,
+        # }
+        # P5: 0.4069  P10: 0.3276 P15: 0.2736
+
+        query_base = (single_query["disease"] + " ") * 3
+        query_gene = (
+            single_query["gene"]
+            .replace("(", " ")
+            .replace(")", " ")
+            .replace(",", "")
+            .replace(";", "")
+            + " "
+        ) * 2
 
         query_body = {
             "query": {
                 "multi_match": {
-                    "query": (single_query["disease"] + ' ' + single_query["gene"].replace("(", " ").replace(")", " ").replace(",", "")).lower(),
+                    "query": (query_base + " " + query_gene).lower(),
                     "type": "cross_fields",
                     "fields": [
                         "brief_title",
@@ -190,30 +229,44 @@ class Query(object):
         )["hits"]["hits"]
         # 获得查询结果
 
-        # print(query_result)
-        # score_max = query_result[0]["_score"]
-        rank = 1
-        with open("trec_eval/eval/r40.txt", "a") as f:
-            try:
-                for qr in query_result:
-                    # 过滤年龄不符合的情况
-                    if "eligibility" in qr["_source"]:
-                        qr_eli = qr["_source"]["eligibility"]
-                        if float(qr_eli["minimum_age"]) > single_query["age"] or\
-                            single_query["age"] > float(qr_eli["maximum_age"]):
-                            continue
-                        if qr_eli["gender"].lower().strip() not in [single_query["gender"].lower(), 'all', 'All']:
-                            print(qr_eli["gender"].lower())
-                            print(single_query["gender"].lower())
-                            continue
+        filter_result = []
 
+        for qr in query_result:
+            # 过滤年龄不符合的情况
+            if "eligibility" in qr["_source"]:
+                qr_eli = qr["_source"]["eligibility"]
+                if float(qr_eli["minimum_age"]) > single_query["age"] or single_query[
+                    "age"
+                ] > float(qr_eli["maximum_age"]):
+                    continue
+                if qr_eli["gender"].lower().strip() not in [
+                    single_query["gender"].lower(),
+                    "all",
+                    "All",
+                ]:
+                    # print(qr_eli["gender"].lower())
+                    # print(single_query["gender"].lower())
+                    continue
+                filter_result.append(qr)
+
+        bm25_Object = calculateBM25(
+            filter_result, (query_base + " " + query_gene).lower(), single_query["id"], name
+        )
+        bm25_Object.run()
+
+        # print(query_result)
+        score_max = query_result[0]["_score"]
+        rank = 1
+        with open("trec_eval/eval/res_{}.txt".format(name), "a") as f:
+            try:
+                for qr in filter_result:
                     # 按照要求格式写文件
                     f.write(
                         "{} Q0 {} {} {} certRI\n".format(
                             single_query["id"],
                             qr["_source"]["id_info"],
                             rank,
-                            round(qr["_score"], 4),
+                            round(qr["_score"] / score_max, 4),
                         )
                     )
                     rank += 1
@@ -233,10 +286,57 @@ class Query(object):
         self.extract_query()
         for single_query in self.extracted:
             print(single_query)
-            self.query(single_query)
+            # self.query(single_query)
 
+    def run_with_5(self):
+        data_5 = [
+            [
+                {'id': '28', 'disease': 'Pancreatic ductal adenocarcinoma', 'gene': 'ERBB3', 'age': 26645, 'gender': 'female', 'other': 'Whipple, FNA'},
+                {'id': '29', 'disease': 'Ampullary carcinoma', 'gene': 'KRAS', 'age': 22265, 'gender': 'male', 'other': 'None'},
+                {'id': '25', 'disease': 'Lung adenocarcinoma', 'gene': 'MET Amplification', 'age': 17520, 'gender': 'male', 'other': 'Emphysema'},
+                {'id': '22', 'disease': 'Lung cancer', 'gene': 'ERBB2 Amplification', 'age': 25550, 'gender': 'male', 'other': 'Arthritis'},
+                {'id': '6', 'disease': 'Melanoma', 'gene': 'NRAS (Q61K)', 'age': 20075, 'gender': 'male', 'other': 'Hypertension'},
+                {'id': '7', 'disease': 'Lung cancer', 'gene': 'EGFR (L858R)', 'age': 18250, 'gender': 'female', 'other': 'Lupus'}
+            ],
+            [
+                {'id': '26', 'disease': 'Breast cancer', 'gene': 'NRAS Amplification', 'age': 12775, 'gender': 'female', 'other': 'None'},
+                {'id': '11', 'disease': 'Gastric cancer', 'gene': 'PIK3CA (E545K)', 'age': 19710, 'gender': 'male', 'other': 'Depression'},
+                {'id': '1', 'disease': 'Liposarcoma', 'gene': 'CDK4 Amplification', 'age': 13870, 'gender': 'male', 'other': 'GERD'},
+                {'id': '18', 'disease': 'Pancreatic cancer', 'gene': 'CDK6 Amplification', 'age': 17520, 'gender': 'male', 'other': 'None'},
+                {'id': '21', 'disease': 'Lung adenocarcinoma', 'gene': 'ALK Fusion', 'age': 23360, 'gender': 'female', 'other': 'Emphysema'},
+                {'id': '4', 'disease': 'Breast cancer', 'gene': 'FGFR1 Amplification, PTEN (Q171)', 'age': 24455, 'gender': 'female', 'other': 'Depression'}
+            ],
+            [
+                {'id': '19', 'disease': 'Colorectal cancer', 'gene': 'FGFR1 Amplification', 'age': 12775, 'gender': 'female', 'other': 'None'},
+                {'id': '24', 'disease': 'Lung cancer', 'gene': 'NTRK1', 'age': 21170, 'gender': 'female', 'other': 'Depression, Hypertension, Diabetes'},
+                {'id': '27', 'disease': 'Pancreatic adenocarcinoma', 'gene': 'KRAS, TP53', 'age': 17885, 'gender': 'female', 'other': 'None'},
+                {'id': '30', 'disease': 'Pancreatic adenocarcinoma', 'gene': 'RB1, TP53, KRAS', 'age': 20805, 'gender': 'female', 'other': 'None'},
+                {'id': '12', 'disease': 'Colon cancer', 'gene': 'BRAF (V600E)', 'age': 12775, 'gender': 'male', 'other': 'None'},
+                {'id': '23', 'disease': 'Breast cancer', 'gene': 'PTEN Loss', 'age': 19710, 'gender': 'female', 'other': 'Congestive Heart Failure'}
+            ],
+            [
+                {'id': '13', 'disease': 'Cholangiocarcinoma', 'gene': 'BRCA2', 'age': 26280, 'gender': 'male', 'other': 'Diabetes'},
+                {'id': '14', 'disease': 'Cholangiocarcinoma', 'gene': 'IDH1 (R132H)', 'age': 23360, 'gender': 'male', 'other': 'Neuropathy'},
+                {'id': '3', 'disease': 'Meningioma', 'gene': 'NF2 (K322), AKT1(E17K)', 'age': 16425, 'gender': 'female', 'other': 'None'},
+                {'id': '16', 'disease': 'Pancreatic cancer', 'gene': 'CDKN2A', 'age': 19710, 'gender': 'male', 'other': 'Diabetes, Hypertension'},
+                {'id': '8', 'disease': 'Lung cancer', 'gene': 'EML4-ALK Fusion transcript', 'age': 18980, 'gender': 'male', 'other': 'Hypertension, Osteoarthritis'},
+                {'id': '9', 'disease': 'Gastrointestinal stromal tumor', 'gene': 'KIT Exon 9 (A502_Y503dup)', 'age': 17885, 'gender': 'female', 'other': 'None'}
+            ],
+            [
+                {'id': '15', 'disease': 'Cervical cancer', 'gene': 'STK11', 'age': 9490, 'gender': 'female', 'other': 'None'},
+                {'id': '20', 'disease': 'Liposarcoma', 'gene': 'MDM2 Amplification', 'age': 9490, 'gender': 'male', 'other': 'None'},
+                {'id': '5', 'disease': 'Melanoma', 'gene': 'BRAF (V600E), CDKN2A Deletion', 'age': 16425, 'gender': 'female', 'other': 'None'},
+                {'id': '10', 'disease': 'Lung adenocarcinoma', 'gene': 'KRAS (G12C)', 'age': 22265, 'gender': 'female', 'other': 'Hypertension, Hypercholesterolemia'},
+                {'id': '17', 'disease': 'Prostate cancer', 'gene': 'PTEN Inactivating', 'age': 29565, 'gender': 'male', 'other': 'Hypertension, Depression'},
+                {'id': '2', 'disease': 'Colon cancer', 'gene': 'KRAS (G13D), BRAF (V600E)', 'age': 18980, 'gender': 'male', 'other': 'Type II Diabetes, Hypertension'}
+            ]
+        ]
+        for i in range(5):
+            for single_query in data_5[i]:
+                self.query(single_query, name = 'cut_' + str(i+1))
 
 if __name__ == "__main__":
     q = Query()
     # q.extract_query()
-    q.run()
+    # q.run()
+    q.run_with_5()
--- Relevance.py
+++ Relevance.py
@@ -9,44 +9,141 @@ from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 import xmltodict
 
+# class BM25(object):
+#     def __init__(self, *args, **kwargs):
+#         self.tokenizer = RegexpTokenizer(r'\w+')
+#         self.lem = WordNetLemmatizer()
+#         self.stopwords = set(stopwords.words('english'))
+#         self.dict = None
+#         self.corpus = None
+#         self.bm25model = None
 
-class BM25(object):
-    def __init__(self, *args, **kwargs):
-        self.tokenizer = RegexpTokenizer(r'\w+')
-        self.lem = WordNetLemmatizer()
-        self.stopwords = set(stopwords.words('english'))
-        self.dict = None
-        self.corpus = None
-        self.bm25model = None
+#     def cleanData(self, doc):
+#         raw_tokens = self.tokenizer.tokenize(doc.lower())
+#         lem_tokens = [self.lem.lemmatize(token) for token in raw_tokens]
+#         lem_tokens_without_stopword = filter(lambda i: i not in self.stopwords, lem_tokens)
+#         return list(lem_tokens_without_stopword)
 
-    def cleanData(self, doc):
-        raw_tokens = self.tokenizer.tokenize(doc.lower())
-        lem_tokens = [self.lem.lemmatize(token) for token in raw_tokens]
-        lem_tokens_without_stopword = filter(lambda i: i not in self.stopwords, lem_tokens)
-        return list(lem_tokens_without_stopword)
+#     def getReady(self, tokens_list):
+#         self.dict = corpora.Dictionary(tokens_list)
 
-    def getReady(self, tokens_list):
-        self.dict = corpora.Dictionary(tokens_list)
+#         print('Dictionary Length:', str(len(self.dict)))
 
-        print('Dictionary Length:', str(len(self.dict)))
-
-        self.corpus = tokens_list
+#         self.corpus = tokens_list
 
 
-class dataClean(object):
-    def __init__(self, xml_path):
-        self.xml_path = xml_path
-        self.doc_list = []
-        self.corpus = None
+# class dataClean(object):
+#     def __init__(self, xml_path):
+#         self.xml_path = xml_path
+#         self.doc_list = []
+#         self.corpus = None
     
-    def oswalk(self):
-        for root, _, files in os.walk(self.xml_path):
-            for xml in files:
-                full_path = os.path.join(root, xml)
-                with open(full_path, 'r') as f:
-                    xmldata = f.read()
-                    dictdata = xmltodict.parse(xmldata)
-                    self.doc_list.append(dictdata)
+#     def oswalk(self):
+#         for root, _, files in os.walk(self.xml_path):
+#             for xml in files:
+#                 full_path = os.path.join(root, xml)
+#                 with open(full_path, 'r') as f:
+#                     xmldata = f.read()
+#                     dictdata = xmltodict.parse(xmldata)
+#                     self.doc_list.append(dictdata)
                     
-                    
+
+class calculateBM25(object):
+    def __init__(self, datasets, query_str, query_id, name="noncut"):
+        self.datasets = datasets
+        self.query = query_str.split(' ')
+        self.query_id = query_id
+        self.corpus = []
+        self.tokenizer = RegexpTokenizer(r'\w+')
+        self.dictionary = None
+        self.sorted_result = []
+        self.bm25Model = None
+        self.name = name
+
+    def getWeightedData(self):
+        score_max = self.datasets[0]["_score"]
+
+        for data in self.datasets:
+            # print(data)
+            # default
+            # brief_title = (data["_source"]["brief_title"] + ' ') * 3
+            # official_title = (data["_source"]["official_title"] + ' ') * 3
+            # detailed_description = (data["_source"]["detailed_description"] + ' ') * 1
+            # brief_summary = (data["_source"]["brief_summary"] + ' ') * 1
+            # criteria = (data["_source"]["eligibility"]["criteria"]["textblock"] + ' ') * 2
+            # keywords = (' '.join(data["_source"]["keyword"]) + ' ') * 3
+            # condition = (' '.join(data["_source"]["condition"]) + ' ') * 2
+            
+            # ss
+            brief_title = (data["_source"]["brief_title"] + ' ') * 4
+            official_title = (data["_source"]["official_title"] + ' ') * 3
+            detailed_description = (data["_source"]["detailed_description"] + ' ') * 1
+            brief_summary = (data["_source"]["brief_summary"] + ' ') * 1
+            criteria = (data["_source"]["eligibility"]["criteria"]["textblock"] + ' ') * 3
+            keywords = (' '.join(data["_source"]["keyword"]) + ' ') * 4
+            condition = (' '.join(data["_source"]["condition"]) + ' ') * 2
+
+            # official_title = (data["_source"]["official_title"] + ' ') * 2
+            # detailed_description = (data["_source"]["detailed_description"] + ' ') * 1
+            # brief_summary = (data["_source"]["brief_summary"] + ' ') * 2
+            # criteria = (data["_source"]["eligibility"]["criteria"]["textblock"] + ' ') * 3
+            # keywords = (' '.join(data["_source"]["keyword"]) + ' ') * 4
+            # condition = (' '.join(data["_source"]["condition"]) + ' ') * 2
+            
+            final_data = brief_title + official_title + detailed_description + brief_summary + criteria + keywords + condition
+            self.corpus.append(self.tokenizer.tokenize((final_data)))
+
+            self.sorted_result.append({"id": self.query_id,"id_info": data["_source"]["id_info"], "score": data["_score"] / score_max})
+
+    def getModel(self):
+        self.dictionary = corpora.Dictionary(self.corpus)
+        self.doc_vectors = [self.dictionary.doc2bow(text) for text in self.corpus]
+        self.bm25Model = bm25.BM25(self.corpus)
+        self.average_idf = sum(map(lambda k: float(self.bm25Model.idf[k]), self.bm25Model.idf.keys())) / len(self.bm25Model.idf.keys())
+
+    def calculateNewResult(self):
+        bm25scores = self.bm25Model.get_scores(self.query, self.average_idf)
+        bm25score_max = max(bm25scores)
+        bm25scores_norm = [s / bm25score_max for s in bm25scores]
+
+        for i in range(10):
+            new_result = []
+            for res_set, bm25s in zip(self.sorted_result, bm25scores_norm):
+                new_res_set = {"id":res_set["id"], "id_info":res_set["id_info"], "score":res_set["score"] * i / 10 + bm25s * (10 - i) /10}
+                new_result.append(new_res_set)
+            
+            filename = 'trec_eval/eval/DE_' + str(i) + '_BM25_' + str(10 - i) + '_' + self.name + '.txt'
+            sorted_new_result = sorted(new_result, key = lambda k:(k.get("score")), reverse=True)
+
+            rank = 1
+            with open(filename, 'a') as f:
+                for r in sorted_new_result:
+                    f.write(
+                        "{} Q0 {} {} {} certRI\n".format(
+                            r["id"],
+                            r["id_info"],
+                            rank,
+                            round(r["score"], 4),
+                        )
+                    )
+                    rank += 1
+            print("Finished {}".format(filename))
+
+    def run(self):
+        self.getWeightedData()
+        self.getModel()
+        self.calculateNewResult()
+
+                
+
+
+
+
+
+        
+
+    
+    
+
+
         
